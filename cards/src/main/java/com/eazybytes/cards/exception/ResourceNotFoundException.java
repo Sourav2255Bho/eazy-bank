66ba960250b8e85e25425ea62c452f09e5d5511e
Sourav2255Bho <bhowmicks2255@gmail.com>
2025-09-29 22:58:40 +0530

    Changes added

--- cards/src/main/java/com/eazybytes/cards/exception/ResourceNotFoundException.java
+++ cards/src/main/java/com/eazybytes/cards/exception/ResourceNotFoundException.java
@@ -3,6 +3,6 @@ package com.eazybytes.cards.exception;
 public class ResourceNotFoundException extends RuntimeException{
 
     public ResourceNotFoundException(String resourceName, String fieldNamme, String fieldValue){
-        super(String.format("@s not found with the given inout data %s : '%s'", resourceName, fieldNamme, fieldValue));
+        super(String.format("Card not found with the given inout data %s : '%s'", resourceName, fieldNamme, fieldValue));
     }
 }
